af7328c87fb716e57f1f2649f8d662f85010f673
matheus <carbonimatheus2@gmail.com>
2025-05-26 18:16:54 -0300

    Projeto para redirecionamento

--- src/pages/RedirectInstagram.jsx
+++ src/pages/RedirectInstagram.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 
 export default function RedirectInstagram() {
   useEffect(() => {
-    window.location.href = "https://www.instagram.com/seu_usuario"; // Substitua pelo seu link
+    window.location.href = "https://www.instagram.com/adbrasrudgeramos"; // Substitua pelo seu link
   }, []);
 
   return (
